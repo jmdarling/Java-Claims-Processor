f32fdf9e72526e58451cb258da2ea9e70bb9aae6
Jonathan Darling <jmdarling@me.com>
2015-04-21 23:02:24 -0500

    Adds the RetrievePolicyProcessor and RetrieveProcedureProcessor. Fixes the RetrieveProviderProcessor.

--- src/utd/claimsProcessing/messageProcessors/RetrieveProcedureProcessor.java
+++ src/utd/claimsProcessing/messageProcessors/RetrieveProcedureProcessor.java
@@ -11,48 +11,48 @@ import javax.jms.Session;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
-import utd.claimsProcessing.dao.ProviderDAO;
+import utd.claimsProcessing.dao.ProcedureDAO;
 import utd.claimsProcessing.domain.Claim;
 import utd.claimsProcessing.domain.ClaimFolder;
-import utd.claimsProcessing.domain.Provider;
+import utd.claimsProcessing.domain.Procedure;
 import utd.claimsProcessing.domain.RejectedClaimInfo;
 
 /**
- * A message processor responsible for retrieving the Provider identified by the Claim
- * from the ProviderDAO. The retrieved provider is attached to the ClaimFolder before
+ * A message processor responsible for retrieving the Procedure identified by the Claim
+ * from the ProcedureDAO. The retrieved procedure is attached to the ClaimFolder before
  * passing to the next step in the process. 
  */
 
-public class RetrieveProviderProcessor extends MessageProcessor implements MessageListener
+public class RetrieveProcedureProcessor extends MessageProcessor implements MessageListener
 {
-	private final static Logger logger = Logger.getLogger(RetrieveProviderProcessor.class);
+	private final static Logger logger = Logger.getLogger(RetrieveProcedureProcessor.class);
 	
 	private MessageProducer producer;
 	
-	public RetrieveProviderProcessor(Session session)
+	public RetrieveProcedureProcessor(Session session)
 	{
 		super(session);
 	}
 
 	public void initialize() throws JMSException
 	{
-		Queue queue = getSession().createQueue(QueueNames.retrievePolicy);
+		Queue queue = getSession().createQueue(QueueNames.routeClaim);
 		producer = getSession().createProducer(queue);
 	}
 
 	public void onMessage(Message message)
 	{
-		logger.debug("RetrieveProviderProcessor ReceivedMessage");
+		logger.debug("RetrieveProcedureProcessor ReceivedMessage");
 
 		try {
 			Object object = ((ObjectMessage) message).getObject();
 			ClaimFolder claimFolder = (ClaimFolder)object;
 			
-			String providerID = claimFolder.getClaim().getProviderID();
-			Provider provider = ProviderDAO.getSingleton().retrieveProvider(providerID);
-			if(provider == null) {
+			String procedureCode = claimFolder.getClaim().getProcedureCode();
+			Procedure procedure = ProcedureDAO.getSingleton().retrieveByCode(procedureCode);
+			if(procedure == null) {
 				Claim claim = claimFolder.getClaim();
-				RejectedClaimInfo rejectedClaimInfo = new RejectedClaimInfo("Provider Not Found: " + providerID);
+				RejectedClaimInfo rejectedClaimInfo = new RejectedClaimInfo("Proceedure Not Found: " + procedureCode);
 				claimFolder.setRejectedClaimInfo(rejectedClaimInfo);
 				if(!StringUtils.isBlank(claim.getReplyTo())) {
 					rejectedClaimInfo.setEmailAddr(claim.getReplyTo());
@@ -60,17 +60,17 @@ public class RetrieveProviderProcessor extends MessageProcessor implements Messa
 				rejectClaim(claimFolder);
 			}
 			else {
-				logger.debug("Found Provider: " + provider.getProviderName());
+				logger.debug("Found Procedure: " + procedure.getProcedureCode());
 
-				claimFolder.setProvider(provider);
+				claimFolder.setProcedure(procedure);
 			
 				Message claimMessage = getSession().createObjectMessage(claimFolder);
 				producer.send(claimMessage);
-				logger.debug("Finished Sending: " + provider.getProviderName());
+				logger.debug("Finished Sending: " + procedure.getProcedureCode());
 			}
 		}
 		catch (Exception ex) {
-			logError("RetrieveProviderProcessor.onMessage() " + ex.getMessage(), ex);
+			logError("RetrieveProcedureProcessor.onMessage() " + ex.getMessage(), ex);
 		}
 	}
 }
